9a1f5823cc28028ff0ee772c9f3acfa4aa7407b2
Morgan Wowk <morgan@boldcommerce.com>
2021-02-28 17:30:25 -0600

    Release v1.0.1

--- cli.rb
+++ cli.rb
@@ -2,8 +2,8 @@ class Cli < Formula
   desc "Painstakingly simple version switching for Composer. Built for teams with many PHP projects."
   homepage "https://github.com/composer-version-manager/cvm"
   version "1.0.0"
-  url "https://github.com/composer-version-manager/cvm/releases/download/1.0.0/cvm-1.0.0.tar.gz"
-  sha256 "cb7664ff858bcdbd07b501e940cddb49fad2c87b20f98ee445e472b451f243da"
+  url "https://github.com/composer-version-manager/cvm/releases/download/1.0.1/cvm-1.0.1.tar.gz"
+  sha256 "0bcc48684644afa376b141316d2c19d6f9728357bacc63c3209f5a411fb71eed"
 
   bottle :unneeded
 
