d1720b8c09bc394c9d0b3398ae46f40f14545278
Morgan Wowk <morgan.wowk@gmail.com>
2021-03-01 12:07:20 -0600

    Release v1.0.5

--- cvm.rb
+++ cvm.rb
@@ -1,9 +1,9 @@
 class Cvm < Formula
   desc "Painstakingly simple version switching for Composer. Built for teams with many PHP projects."
   homepage "https://github.com/composer-version-manager/cvm"
-  version "1.0.3"
-  url "https://github.com/composer-version-manager/cvm/releases/download/1.0.3/cvm-1.0.3.tar.gz"
-  sha256 "75bd8962fd2685bbcb48bf61b0fc581e91d66da959209ac1f0a0d090000021c2"
+  version "1.0.5"
+  url "https://github.com/composer-version-manager/cvm/releases/download/1.0.5/cvm-1.0.5.tar.gz"
+  sha256 "60b44f10d3a8e93d5e08c876063e610feef64840d6b01cdd56764780b72916e1"
 
   bottle :unneeded
 
